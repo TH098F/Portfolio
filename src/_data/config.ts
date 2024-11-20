7c89d66485ab8a1f55d1df1a25d3fe8d6ef5e375
TH098F <TH098f@proton.me>
2024-11-20 21:22:26 +0100

    feat: added dynamic colors everywhere + added proj previews

--- src/_data/config.ts
+++ src/_data/config.ts
@@ -33,18 +33,20 @@ const config: Config = {
     projects: [
         {
             name: "Arch Engine", tags: [ProjectTag.ClosedSource, ProjectTag.Graphics], technologies: [Skill.Cpp, Skill.C, Skill.VSCode],
-            desc: "\"Arch\" is an API-agnostic rendering engine built on top of Vulkan and DirectX11. I plan to extend it to DirectX12 and OpenGL in the future. Arch was originally just a way for me to teach myself Vulkan, but i later turned it into a whole project and added DirectX11 support. The pure Vulkan version was written in C, but I later changed to C++ for it's features like 'methods'"
+            desc: "Arch is an API-agnostic rendering engine built on top of Vulkan and DirectX11. I plan to extend it to DirectX12 and OpenGL in the future. Arch was originally me teaching myself Vulkan, but I later turned it into a whole project and added DirectX11 support. The Vulkan only version was written in C, but I later changed to C++ for it's OOP features",
+            preview: "portfolio_v2/archPreview.png"
         },
         {
             name: "Portfolio", tags: [ProjectTag.OpenSource, ProjectTag.WebDev], technologies: [Skill.TypeScript, Skill.Astro, Skill.CSS],
-            desc: "My Portfolio is a fork of theMelon's portfolio. I wanted to turn his portfolio into a dynamic version which could then be used as an easy to use 'template'.\""
+            desc: "This Portfolio is a fork of theMelon's portfolio. I wanted to turn his portfolio into a dynamic version which could then be used as an easy to use 'template'.",
+            preview: "portfolio_v2/portfolioPreview.png"
         }
     ],
     links: {
         github: "https://github.com/TH098F",
         email: "th098f@proton.me"
     },
-    typewrite_words: ["Hello I'm TH098","It's pronounced: Theo","Cinnamon Rolls"]
+    typewrite_words: ["Hello I'm TH098","It's pronounced 'Theo'","Cinnamon Rolls"]
 };
 
 export default config;
