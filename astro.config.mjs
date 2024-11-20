48635627150a2e2a8b5fc07746002dec833b6782
TH098F <TH098f@proton.me>
2024-11-20 14:51:38 +0100

    fix: gh pages issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import config from './src/_data/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: config.url ? config.url : 'http://127.0.0.1'
+    site: config.url ? config.url : 'http://127.0.0.1',
+    base: 'portfolio_v2'
 });
